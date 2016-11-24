1793ae07d4c62460f2cc177cb5016641ecc41ae3
rishita7 <lifeoflight18@gmail.com>
2016-11-23 21:02:36 -0800

    Update MessagesController.cs

--- contosoBot/Controllers/MessagesController.cs
+++ contosoBot/Controllers/MessagesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -7,6 +7,8 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using contosoBot.Services; 
 
 namespace contosoBot
 {
@@ -14,51 +16,75 @@ namespace contosoBot
     public class MessagesController : ApiController
     {
 
+
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
+        /// the bot is running and we arnt holding up the bot while we're waiting for the API responce
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity) // This is a http post that is looking for data 
-            // from the body 
+                                                                                 // from the body 
+                                                                                 // the message controller is passing the whole activity class
         {
             if (activity.Type == ActivityTypes.Message) // here the activity is a message 
             {
-                // WRITE CODE HERE 
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
                 // The connector client is equating the variable connector to a new connector client with a uri that does the activity 
-                // service 
-                var stockSymbol = activity.Text;
-                var replyMessage = await GetStock(stockSymbol);
+                // WRITE CODE HERE 
+                contosoBot.Services.StockItem stock1;
+               
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply(replyMessage); // we need to send back the reply message Thats what
-                // activity reply is doing here. 
-                await connector.Conversations.ReplyToActivityAsync(reply); 
-                // here we are connecting to the connecter variable we created earlier using the connector client 
-                
+        HttpClient client = new HttpClient();
+                string rawData = await client.GetStringAsync(new Uri("http://dev.markitondemand.com/Api/v2/Quote/json?symbol=" + activity.Text));
+                stock1 = JsonConvert.DeserializeObject<StockItem>(rawData);
+
+                string name = stock1.Name;
+                float ChangePercentYTD = stock1.ChangePercentYTD;
+                string Time = stock1.Timestamp;
+                float MarketCap = stock1.MarketCap;
+                float LastPrice = stock1.LastPrice;
+                float Open = stock1.Open;
+                string symbol = stock1.Symbol;
+
+                // adding the HERO CARD
+                Activity stockReply = activity.CreateReply($"Stock for the {name}");
+                stockReply.Recipient = activity.From;
+                stockReply.Type = "message";
+                stockReply.Attachments = new List<Attachment>();
+
+                List<CardImage> cardImages = new List<CardImage>();
+                cardImages.Add(new CardImage(url: "http://megaicons.net/static/img/icons_title/31/98/title/ios7-stock-icon.png"));
+
+                List<CardAction> cardButtons = new List<CardAction>();
+                CardAction plButton = new CardAction()
+                {
+                    Value = "http://www.nasdaq.com/symbol/" + symbol + "/interactive-chart",
+                    Type = "openUrl",
+                    Title = "More Info"
+                };
+                cardButtons.Add(plButton);
+
+
+
+                ThumbnailCard plCard = new ThumbnailCard()
+                {
+                    Title = name + ":",
+                    Subtitle = " On " + Time + " Stock Price is: $" + LastPrice + "  With a market value of: " + MarketCap + "  And its Change in Year to Date percentage is: " + ChangePercentYTD + "%",
+                    Images = cardImages,
+                    Buttons = cardButtons
+                };
+
+                Attachment plAttachment = plCard.ToAttachment();
+                stockReply.Attachments.Add(plAttachment);
+                await connector.Conversations.SendToConversationAsync(stockReply);
             }
+
             else
             {
                 HandleSystemMessage(activity);
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
-        }
-
-        // creating a small function 
-        private async Task<string> GetStock(string symbol)
-        {
-            var stockItem = await Sockservices.Stock.GetStockPrice(symbol);
-            string returnValue;
-            if("FAIL" == stockItem.Status)
-            {
-                returnValue = $"I'm sorry I could not find any stock with symbol '{symbol}'";
-            }
-            else
-            {
-                returnValue = $"Stock: {stockItem.Symbol}, Price: ${stockItem.LastPrice}"; 
-            }
-            return returnValue; 
         }
 
         private Activity HandleSystemMessage(Activity message)
